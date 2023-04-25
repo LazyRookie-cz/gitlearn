c80e0f03fe117554c0d2e01f39a63c0240102d97
chenzeng <chenzeng@travelsky.com.cn>
2023-04-25 16:33:30 +0800

    feat：继续测试rebase

--- src/main/java/com/example/gitstudy/gitlearn/RebaseOnTest.java
+++ src/main/java/com/example/gitstudy/gitlearn/RebaseOnTest.java
@@ -11,5 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 public class RebaseOnTest {
     public static void main(String[] args) {
         System.out.println("test1");
+        System.out.println("test2");
     }
 }
