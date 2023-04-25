9086b53f5d39b45bd0028107875fc9a2731f1dc5
chenzeng <chenzeng@travelsky.com.cn>
2023-04-25 16:40:28 +0800

    feat：addtest 测试

--- src/main/java/com/example/gitstudy/ServletInitializer.java
+++ src/main/java/com/example/gitstudy/ServletInitializer.java
@@ -7,6 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        System.out.println(1);
         return application.sources(GitstudyApplication.class);
     }
 
