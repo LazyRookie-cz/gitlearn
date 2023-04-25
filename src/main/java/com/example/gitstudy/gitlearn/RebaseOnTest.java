0cb324e848ca83efe459df1713c18007e0b0b612
chenzeng <chenzeng@travelsky.com.cn>
2023-04-25 16:33:04 +0800

    feat：增加rebase测试类

---
+++ src/main/java/com/example/gitstudy/gitlearn/RebaseOnTest.java
@@ -0,0 +1,15 @@
+package com.example.gitstudy.gitlearn;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Description:
+ * @Author: chenzeng
+ * @Date: 2023年04月25日 16:31
+ */
+@Slf4j
+public class RebaseOnTest {
+    public static void main(String[] args) {
+        System.out.println("test1");
+    }
+}
